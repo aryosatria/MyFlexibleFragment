96944ef5af97bf89a61a31cae85a70c0877bdb41
satria <aryozatria04@gmail.cpm>
2020-11-30 17:35:49 +0700

    How to make FlexibleFragment

---
+++ app/src/main/java/com/example/myflexiblefragment/CategoryFragment.java
@@ -0,0 +1,59 @@
+package com.example.myflexiblefragment;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class CategoryFragment extends Fragment implements View.OnClickListener {
+
+
+    public CategoryFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_category, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnDetailCategory = view.findViewById(R.id.btn_detail_category);
+        btnDetailCategory.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_detail_category) {
+            DetailCategoryFragment mDetailCategoryFragment = new DetailCategoryFragment();
+
+            Bundle mBundle = new Bundle();
+            mBundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle");
+            String description = "Kategori ini akan berisi produk_produk lifestyle";
+
+            mDetailCategoryFragment.setArguments(mBundle);
+            mDetailCategoryFragment.setDescription(description);
+
+            FragmentManager mFragmentManager = getFragmentManager();
+            if (mFragmentManager != null) {
+                mFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_container, mDetailCategoryFragment, DetailCategoryFragment.class.getSimpleName())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        }
+    }
+}
